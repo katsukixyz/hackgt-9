593cc7fb61f377bf85ee2675cbbcd01237816b67
Katsuki Chan <katsukidotxyz@gmail.com>
2022-10-22 16:28:01 -0400

    mid-fidelity sample styling

--- scripts/content.js
+++ scripts/content.js
@@ -1,6 +1,5 @@
 setTimeout(() => {
   const calendar = document.querySelector('[jscontroller="JjlYBf"]');
-  console.log(calendar);
   if (calendar) {
     calendar.addEventListener("click", (event) => {
       setTimeout(() => {
@@ -20,14 +19,11 @@ setTimeout(() => {
         weatherPopup.style.backgroundColor = "white";
         weatherPopup.style.boxShadow =
           "0px 24px 38px 3px rgb(0 0 0 / 14%), 0px 9px 46px 8px rgb(0 0 0 / 12%), 0px 11px 15px -7px rgb(0 0 0 / 20%)";
-        weatherPopup.style.left = "-210px";
+        weatherPopup.style.left = "-220px";
         weatherPopup.style.display = "flex";
         weatherPopup.style.flexDirection = "column";
         weatherPopup.style.overflow = "auto";
-        weatherPopup.style.borderRadius = "8px";
         weatherPopup.style.pointerEvents = "visible";
-
-        weatherPopup.addEventListener("click", () => console.log("clicked!"));
 
         //Adding hour elements to the pop up
         const hourArray = [];
@@ -35,17 +31,47 @@ setTimeout(() => {
           hourArray.push([i, 30]);
         }
 
-        let hourW = document.createElement("p");
-        hourW.textContent =
-          hourArray[0][0] + "F " + hourArray[0][1] + "% chance";
-        weatherPopup.appendChild(hourW);
+        const dayW = document.createElement("div");
+        hourArray.forEach((hour) => {
+          const hourW = document.createElement("div");
+          hourW.style.display = "flex";
+          hourW.style.flexDirection = "row";
+          hourW.style.alignItems = "center";
+          hourW.style.justifyContent = "space-between";
 
-        for (let i = 1; i < 24; i++) {
-          hourW = document.createElement("p");
-          hourW.textContent =
-            hourArray[i][0] + "F " + hourArray[i][1] + "% chance";
-          weatherPopup.appendChild(hourW);
-        }
+          const time = document.createElement("p");
+          time.textContent = "2:00pm";
+
+          const icon = document.createElement("div");
+          icon.style.display = "flex";
+          icon.style.flexDirection = "column";
+          icon.style.alignItems = "center";
+
+          const iconImage = document.createElement("img");
+          iconImage.src = chrome.runtime.getURL("images/rain_s_sunny.png");
+          iconImage.style.height = "25px";
+          iconImage.style.border = "none";
+          iconImage.style.padding = 0;
+          iconImage.style.margin = 0;
+          const iconPercent = document.createElement("p");
+          iconPercent.style.fontSize = "12px";
+          iconPercent.style.border = "none";
+          iconPercent.style.padding = 0;
+          iconPercent.style.margin = 0;
+          iconPercent.textContent = "30%";
+          icon.appendChild(iconImage);
+          icon.appendChild(iconPercent);
+
+          const temp = document.createElement("p");
+          temp.textContent = "47°";
+
+          hourW.appendChild(time);
+          hourW.appendChild(icon);
+          hourW.appendChild(temp);
+
+          dayW.appendChild(hourW);
+        });
+        weatherPopup.appendChild(dayW);
 
         //Weather button styling
         const weatherButton = document.createElement("button");
