0bc8fd9d5a4009d9db3d7fdcfb47150cbbddc795
Katsuki Chan <katsukidotxyz@gmail.com>
2022-10-22 14:15:14 -0400

    fixed promise issue

--- scripts/weatherRequest.js
+++ scripts/weatherRequest.js
@@ -1,66 +1,78 @@
-const printCoords = async (locationInput) => {
+// const printCoords = async (locationInput) => {
+//   locURL =
+//     "https://nominatim.openstreetmap.org/search?q=" +
+//     locationInput.replace(" ", "+") +
+//     "&format=xml&addressdetails=1";
+//   const location = await fetch(locURL)
+//     .then((response) => response.text())
+//     .then((str) => new DOMParser().parseFromString(str, "text/xml"));
 
-    locURL = "https://nominatim.openstreetmap.org/search?q=" + locationInput.replace(" ", "+") + "&format=xml&addressdetails=1"
-    const location = await fetch(locURL)
-        .then(response => response.text())
-        .then(str => new DOMParser().parseFromString(str, "text/xml"));
-    
-    let coordinate = new Array();
-    const lat = location.getElementsByTagName("place")[0].getAttribute("lat");
-    coordinate.push(lat);
-    const lon = location.getElementsByTagName("place")[0].getAttribute("lon");
-    coordinate.push(lon);
-    return coordinate;
-}
+//   let coordinate = new Array();
+//   const lat = location.getElementsByTagName("place")[0].getAttribute("lat");
+//   coordinate.push(lat);
+//   const lon = location.getElementsByTagName("place")[0].getAttribute("lon");
+//   coordinate.push(lon);
+//   return coordinate;
+// };
 
-const printWeather = async (latitude, longitude) => {
-    url = "https://forecast.weather.gov/MapClick.php?" + "lat=" + latitude.toFixed(4) + "&lon=" + longitude.toFixed(4) + "&FcstType=digitalDWML";
+// const printWeather = async (latitude, longitude) => {
+//   url =
+//     "https://forecast.weather.gov/MapClick.php?" +
+//     "lat=" +
+//     latitude.toFixed(4) +
+//     "&lon=" +
+//     longitude.toFixed(4) +
+//     "&FcstType=digitalDWML";
 
-    // get xml of website with data
-    const weather = await fetch(url)
-            .then(response => response.text())
-            .then(str => new DOMParser().parseFromString(str, "text/xml"));
-    
-    let time = new Array();
+//   // get xml of website with data
+//   const weather = await fetch(url)
+//     .then((response) => response.text())
+//     .then((str) => new DOMParser().parseFromString(str, "text/xml"));
 
-    const dateTimes = weather.getElementsByTagName("time-layout")[0].childNodes;
-    dateTimes.forEach((node) => {
-        if (node.tagName === "start-valid-time") {
-            time.push(node['innerHTML'])
-        }    
-    })
+//   let time = new Array();
 
-    let precip = new Array();
+//   const dateTimes = weather.getElementsByTagName("time-layout")[0].childNodes;
+//   dateTimes.forEach((node) => {
+//     if (node.tagName === "start-valid-time") {
+//       time.push(node["innerHTML"]);
+//     }
+//   });
 
-    const chanceOfPrecip = weather.getElementsByTagName("probability-of-precipitation")[0].childNodes;
-    chanceOfPrecip.forEach((node) => {
-        precip.push(parseInt(node['innerHTML']));
-    })
+//   let precip = new Array();
 
-    let temperature = new Array();
+//   const chanceOfPrecip = weather.getElementsByTagName(
+//     "probability-of-precipitation"
+//   )[0].childNodes;
+//   chanceOfPrecip.forEach((node) => {
+//     precip.push(parseInt(node["innerHTML"]));
+//   });
 
-    const temperatureValues = weather.getElementsByTagName("temperature")[2].childNodes;
-    temperatureValues.forEach((node) => {
-        temperature.push(parseInt(node['innerHTML']));
-    })
-    
-    let combined = time.map((e, i) => {
-        return [e, temperature[i], precip[i]];
-    });
+//   let temperature = new Array();
 
-    return combined;
-}
+//   const temperatureValues =
+//     weather.getElementsByTagName("temperature")[2].childNodes;
+//   temperatureValues.forEach((node) => {
+//     temperature.push(parseInt(node["innerHTML"]));
+//   });
 
-function success(position) {
-    const crd = position.coords;
+//   let combined = time.map((e, i) => {
+//     return [e, temperature[i], precip[i]];
+//   });
 
-}
+//   return combined;
+// };
 
-const getCurrentLocation = async () => {
-    return navigator.geolocation.getCurrentPosition(success);
-}
-console.log("temp");
-getCurrentLocation();
-//printCoords("Atlanta, GA").then(data => console.log(data));
+// function success(position) {
+//   const crd = position.coords;
+// }
 
-export { printCoords, printWeather, getCurrentLocation };
+// const getCurrentLocation = async () => {
+//   navigator.geolocation.getCurrentPosition((position) => {
+//     const { latitude, longitude } = position.coords;
+//     return printWeather(latitude, longitude).then((d) => d);
+//   });
+// };
+
+// //printCoords("Atlanta, GA").then(data => console.log(data));
+
+// // export { printCoords, printWeather, getCurrentLocation };
