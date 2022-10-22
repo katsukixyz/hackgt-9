b6620d39a98a2c55837580948ce06365012634f4
Katsuki Chan <katsukidotxyz@gmail.com>
2022-10-21 23:55:19 -0400

    extract month and date

--- scripts/content.js
+++ scripts/content.js
@@ -5,7 +5,10 @@ setTimeout(() => {
     calendar.addEventListener("click", (event) => {
       setTimeout(() => {
         const dateElements = document.querySelectorAll(".ky6s2b");
-        console.log(dateElements);
+        const date = new Date(dateElements[0].textContent.split(", ")[1]);
+        const month = date.getMonth() + 1;
+        const day = date.getDate();
+        console.log(month, day);
       }, 500);
     });
   }
