6381c28d1cdc9945c2ded5802821ebc2c3531713
jagrinberg <jacob@grinberg.net>
2022-10-22 14:32:00 -0400

    don't have to wait 5 seconds

--- scripts/content.js
+++ scripts/content.js
@@ -1,58 +1,64 @@
-setTimeout(() => {
-  const calendar = document.querySelector('[jscontroller="JjlYBf"]');
-  console.log(calendar);
-  if (calendar) {
-    calendar.addEventListener("click", (event) => {
-      setTimeout(() => {
-        document.querySelector(".RDlrG").style.overflowX = "visible";
-        document.querySelector(".RDlrG").style.overflowY = "visible";
-        const dateElements = document.querySelectorAll(".ky6s2b");
-        const titleElement = document.querySelector(".mvRfff");
+exists = false;
 
-        //weatherPopup styling
-        const weatherPopup = document.createElement("div");
-        weatherPopup.style.position = "absolute";
-        weatherPopup.style.padding = "20px";
-        weatherPopup.style.height = "250px";
-        weatherPopup.style.width = "180px";
-        weatherPopup.style.zIndex = 20;
-        weatherPopup.style.backgroundColor = "lightblue";
-        weatherPopup.style.left = "-210px";
-        weatherPopup.style.display = "flex";
-        weatherPopup.style.flexDirection = "column";
-        weatherPopup.style.overflow = "auto";
-        weatherPopup.style.borderRadius = "8px";
+const listenForEvent = () => {
+  if (document.querySelector(".RDlrG") && !exists) {
+    document.querySelector(".RDlrG").style.overflowX = "visible";
+    document.querySelector(".RDlrG").style.overflowY = "visible";
+    const dateElements = document.querySelectorAll(".ky6s2b");
+    const titleElement = document.querySelector(".mvRfff");
 
-        //Adding hour elements to the pop up
-        let hourW = document.createElement("p");
-        hourW.textContent = "Temp, Percent chance";
-        weatherPopup.appendChild(hourW);
+    //weatherPopup styling
+    const weatherPopup = document.createElement("div");
+    weatherPopup.style.position = "absolute";
+    weatherPopup.style.padding = "20px";
+    weatherPopup.style.height = "250px";
+    weatherPopup.style.width = "180px";
+    weatherPopup.style.zIndex = 20;
+    weatherPopup.style.backgroundColor = "lightblue";
+    weatherPopup.style.left = "-210px";
+    weatherPopup.style.display = "flex";
+    weatherPopup.style.flexDirection = "column";
+    weatherPopup.style.overflow = "auto";
+    weatherPopup.style.borderRadius = "8px";
 
-        for (let i = 0; i < 15; i++) {
-          hourW = document.createElement("p");
-          hourW.textContent = "Temp, Percent chance";
-          weatherPopup.appendChild(hourW);
-        }
+    //Adding hour elements to the pop up
+    let hourW = document.createElement("p");
+    hourW.textContent = "Temp, Percent chance";
+    weatherPopup.appendChild(hourW);
 
-        //Weather button styling
-        const weatherButton = document.createElement("button");
-        weatherButton.textContent = "☁️";
-        weatherButton.style.position = "absolute";
-        weatherButton.style.background = "rgba(255, 122, 89, 0)";
-        weatherButton.style.borderStyle = "none";
-        weatherButton.style.bottom = "400px";
-        weatherButton.style.left = "20px";
-        weatherButton.addEventListener("click", (wClickEvent) => {
-          titleElement.appendChild(weatherPopup);
-        });
+    for (let i = 0; i < 15; i++) {
+      hourW = document.createElement("p");
+      hourW.textContent = "Temp, Percent chance";
+      weatherPopup.appendChild(hourW);
+    }
 
-        titleElement.appendChild(weatherButton);
-
-        const date = new Date(dateElements[0].textContent.split(", ")[1]);
-        const month = date.getMonth() + 1;
-        const day = date.getDate();
-        console.log(month, day);
-      }, 500);
+    //Weather button styling
+    const weatherButton = document.createElement("button");
+    weatherButton.textContent = "☁️";
+    weatherButton.style.position = "absolute";
+    weatherButton.style.background = "rgba(255, 122, 89, 0)";
+    weatherButton.style.borderStyle = "none";
+    weatherButton.style.bottom = "400px";
+    weatherButton.style.left = "20px";
+    weatherButton.addEventListener("click", (wClickEvent) => {
+      titleElement.appendChild(weatherPopup);
     });
+
+    titleElement.appendChild(weatherButton);
+
+    const date = new Date(dateElements[0].textContent.split(", ")[1]);
+    const month = date.getMonth() + 1;
+    const day = date.getDate();
+    console.log(month, day);
+    exists = true;
+  } else {
+    if (!document.querySelector(".RDlrG")) {
+      exists = false;
+    }
   }
-}, 5000);
+
+  setTimeout(listenForEvent, 250);
+}
+
+listenForEvent();
+
