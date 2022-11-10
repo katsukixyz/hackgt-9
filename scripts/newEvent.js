3761f79ee58c96956c33c3d08aa5425b93fb4af9
Joseph <campbellbjoseph@gmail.com>
2022-11-09 22:53:33 -0500

    Moved main loop to content.js, changed content.js->newEvent.js, content2.js->oldEvent.js

--- scripts/newEvent.js
+++ scripts/newEvent.js
@@ -1,58 +1,44 @@
-exists = false;
-eventType = null;
-
 const curWeatherPopup = new WeatherPopup();
 
-/**
- * 
- * @returns Array with two values, if box exists output[0] is true and output[1] is the type of event (old or new). If box does not exist,
- * then output[0] is false and output[1] is null.
- */
 const checkNewEvent = async () => {
   const box = document.querySelector(".RDlrG");
-  
+
   if (!box) {
-    return [false, null];
+    return false;
   }
 
   if (box.getAttribute("data-allow-wheel-scroll") === "false") {
-    return [true, "old"];
+    return false;
   } else {
-    return [true, "new"];
+    return true;
   }
 }
 
-const updatePopup = async (lat, lon, eType) => {
+const updatePopup = async (lat, lon) => {
   const dialogPopup = document.querySelector(".RDlrG");
   dialogPopup.style.overflowX = "visible";
   dialogPopup.style.overflowY = "visible";
   dialogPopup.style.position = "relative";
   dialogPopup.appendChild(curWeatherPopup.weatherPopup);
 
-
-
-  if (eType === "new") {
-    let titleElement = document.querySelector(".mvRfff");
-    let dateElement = document.querySelector(".ky6s2b");
-    let locationBox = document.querySelector('[aria-label="Location"]');
-  } else {
-    
-  }
+  const titleElement = document.querySelector(".mvRfff");
+  const dateElement = document.querySelector(".ky6s2b");
+  const locationBox = document.querySelector('[aria-label="Location"]');
 
   const observerOptions = {
     attributes: true,
   };
 
   const observerDate = new MutationObserver(async (mutationList, observer) => {
-    const date = new Date(mutationList[mutationList.length-1].target.textContent.split(", ")[1]);
+    const date = new Date(mutationList[mutationList.length - 1].target.textContent.split(", ")[1]);
 
     curWeatherPopup.updateDateWeather(date);
   });
   observerDate.observe(dateElement, observerOptions);
 
   const observerLoc = new MutationObserver(async (mutationList, observer) => {
-    console.log(mutationList[mutationList.length-1]);
-    let address = mutationList[mutationList.length-1].target.value.split(", ");
+    console.log(mutationList[mutationList.length - 1]);
+    let address = mutationList[mutationList.length - 1].target.value.split(", ");
     while (address.length > 4) {
       address.shift();
     }
@@ -65,26 +51,9 @@ const updatePopup = async (lat, lon, eType) => {
     }
   });
   observerLoc.observe(locationBox, observerOptions);
-  
+
   const date = new Date(dateElement.textContent.split(", ")[1]);
 
   curWeatherPopup.updateWeather(lat, lon, date)
   titleElement.appendChild(curWeatherPopup.weatherButton);
 };
-
-const listenForEvent = async () => {
-  if ((await checkNewEvent())[0] && !exists) {
-    const [currentLat, currentLong] = await getCurrentLocation.then((data) => data);
-
-    updatePopup(currentLat, currentLong);
-
-    exists = true;
-  } else {
-    if ((await checkNewEvent())[0] === false) {
-      exists = false;
-    }
-  }
-  setTimeout(listenForEvent, 250);
-};
-
-listenForEvent();
--- scripts/oldEvent.js
+++ scripts/oldEvent.js
@@ -1,10 +1,8 @@
-existsOld = false;
-
 const oldWeatherPopup = new WeatherPopup();
 
 const checkOldEvent = async () => {
   const box = document.querySelector(".RDlrG");
-  
+
   if (!box) {
     return false;
   }
@@ -25,38 +23,9 @@ const updatePopupOld = async (lat, lon) => {
 
   const titleElement = document.querySelector(".wv9rPe");
   const dateElement = document.querySelector(".DN1TJ");
-  
+
   const date = new Date(dateElement.textContent.split(", ")[1].split("⋅")[0]);
 
   oldWeatherPopup.updateWeather(lat, lon, date)
   titleElement.appendChild(oldWeatherPopup.weatherButton);
 };
-
-const listenForOldEvent = async () => {
-  if ((await checkOldEvent()) && !existsOld) {
-    const locat = document.querySelectorAll(".DN1TJ")[1];
-
-    let address = locat.textContent.split(", ");
-    while (address.length > 4) {
-      address.shift();
-    }
-    let [lat, long] = await getCurrentLocation.then((data) => data);
-    if (address.length !== 0) {
-      let coord = await printCoords(address.join(", "));
-      if (coord) {
-        [lat, long] = coord;
-      }
-    }
-
-    updatePopupOld(lat, long);
-
-    existsOld = true;
-  } else {
-    if (!(await checkOldEvent())) {
-      existsOld = false;
-    } 
-  }
-  setTimeout(listenForOldEvent, 250);
-};
-
-listenForOldEvent();
